b8708ebad8e27bee7d1edd24550b59f7639c7c9a
Elior <ezur6294@gmail.com>
2020-01-14 21:17:15 +0100

    LINK ADDED

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import Home from './views/Home'
 import Login from './views/Login'
 import { Route, Switch } from 'react-router-dom'
-
+import { DoodoHead } from "./cmps/DoodoHead";
 function Router() {
     return (
             <Switch>
                 <Route exact path="/" component={Login} />
                 <Route exact path="/home" component={Home} />
+                <Route exact path = "/game/doodohead" component={DoodoHead} />
             </Switch>
     );
 }
---
+++ src/cmps/DoodoHead.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { connect } from 'tls';
+
+class DoodoHead extends React.Component {
+    state = {
+    }
+    async componentDidMount() {
+
+    }
+
+    render() {
+        return (
+            <>
+            <h1>DoodoHead</h1>
+            </>
+        )
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+    }
+}
+
+const mapDispatchToProps = {
+}
+
+export default connect(
+
+)(DoodoHead)
